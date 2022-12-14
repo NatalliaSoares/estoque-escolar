d9524575f45c52f989318c9e70aeaafd9eb468e8
NatalliaSoares <natallia.soares31@gmail.com>
2022-12-14 19:27:30 -0300

    Banco de Dados

--- db/banco.sql
+++ db/banco.sql
@@ -25,23 +25,23 @@ CREATE TABLE IF NOT EXISTS `cardapio` (
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `nome_cardapio` varchar(100) NOT NULL,
   PRIMARY KEY (`id_cardapio`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela estoque_escolar.cardapio: 23 rows
+-- Copiando dados para a tabela estoque_escolar.cardapio: 35 rows
 /*!40000 ALTER TABLE `cardapio` DISABLE KEYS */;
 INSERT INTO `cardapio` (`id_cardapio`, `status`, `nome_cardapio`) VALUES
 	(1, 1, 'galinhada'),
 	(2, 1, 'galinhada'),
 	(3, 1, 'galinhada'),
-	(4, 1, 'feijoada'),
-	(5, 1, 'arroz'),
+	(4, 0, 'feijoada'),
+	(5, 1, 'fffffffff'),
 	(6, 0, 'Arroz com carne moida'),
-	(7, 0, ''),
+	(7, 0, 'jjjjjj'),
 	(8, 0, 'Frango com arroz'),
 	(9, 0, 'Lasanha'),
 	(10, 0, 'FeijÃ£o com linguiÃ§a'),
 	(11, 0, 'sfdsd'),
-	(12, 0, ''),
+	(12, 0, 'ssssss'),
 	(13, 0, 'Galinhada'),
 	(14, 0, 'Galinhada'),
 	(15, 0, 'sdfgdfg'),
@@ -52,7 +52,19 @@ INSERT INTO `cardapio` (`id_cardapio`, `status`, `nome_cardapio`) VALUES
 	(20, 0, 'sdfjds'),
 	(21, 0, 'sdfjds'),
 	(22, 0, 'sdfs'),
-	(23, 0, 'Galinhada');
+	(23, 0, 'Galinhada'),
+	(24, 0, 'Lasanha'),
+	(25, 0, 'huukhu'),
+	(26, 0, 'Frango'),
+	(27, 0, 'Estrogonofe'),
+	(28, 0, 'Vaca Atolada'),
+	(29, 0, 'sdfsdg'),
+	(30, 0, 'Galinhada'),
+	(31, 1, 'churrasco'),
+	(32, 1, 'LinguiÃ§a'),
+	(33, 1, 'Lasanha'),
+	(34, 1, 'fddfg'),
+	(35, 1, 'Farofa com arroz');
 /*!40000 ALTER TABLE `cardapio` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela estoque_escolar.entrada_nota
@@ -68,28 +80,13 @@ CREATE TABLE IF NOT EXISTS `entrada_nota` (
   `total_nota` float(10,3) NOT NULL DEFAULT '0.000',
   `status` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`id_nota`)
-) ENGINE=MyISAM AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela estoque_escolar.entrada_nota: 17 rows
+-- Copiando dados para a tabela estoque_escolar.entrada_nota: 2 rows
 /*!40000 ALTER TABLE `entrada_nota` DISABLE KEYS */;
 INSERT INTO `entrada_nota` (`id_nota`, `data_entrada`, `id_fornecedor`, `numero_nota`, `id_produto`, `qtn_produto`, `id_recurso`, `valor_produto`, `total_nota`, `status`) VALUES
-	(2, '2022-10-25 22:56:13', 3, '35', 0, 0, 0, 0.000, 200.000, 0),
-	(3, '2022-10-25 22:56:50', 3, '35', 0, 0, 0, 0.000, 200.000, 0),
-	(4, '2022-10-25 22:58:07', 5, '65', 0, 0, 0, 0.000, 855.000, 0),
-	(5, '2022-10-25 22:58:32', 7, '95', 0, 0, 0, 0.000, 85.000, 0),
-	(6, '2022-11-03 22:31:26', 1, '4', 0, 0, 435, 0.000, 435.000, 0),
-	(7, '2022-11-03 22:35:54', 1, '345', 0, 0, 656, 0.000, 66.000, 0),
-	(8, '2022-11-03 22:36:25', 1, '345', 0, 0, 656, 0.000, 66.000, 0),
-	(9, '2022-11-04 23:28:39', 5, '5', 0, 0, 4, 0.000, 1233.000, 0),
-	(10, '2022-11-07 22:20:43', 7, '78', 0, 0, 1, 0.000, 100.000, 0),
-	(11, '2022-11-08 22:47:20', 3, '3', 0, 0, NULL, 0.000, 120.000, 1),
-	(12, '2022-11-10 21:49:52', 3, '3', 0, 0, NULL, 0.000, 1233.000, 1),
-	(13, '2022-11-10 21:57:29', 3, '2', 0, 0, NULL, 0.000, 3435.000, 1),
-	(14, '2022-11-10 22:02:06', 3, '4', 0, 0, NULL, 0.000, 325.000, 1),
-	(15, '2022-11-10 22:10:48', 8, '3', 0, 0, NULL, 0.000, 4546.000, 0),
-	(16, '2022-11-10 22:11:27', 9, '3', 0, 0, NULL, 0.000, 456.000, 0),
-	(17, '2022-11-10 22:12:01', 7, '5', 0, 0, NULL, 0.000, 567.000, 0),
-	(18, '2022-11-16 21:56:09', 3, '34', 0, 0, NULL, 0.000, 354.000, 0);
+	(25, '2022-12-13 22:53:24', 4, '125', 0, 0, 2, 0.000, 200.000, 0),
+	(24, '2022-12-13 22:50:18', 9, '1234', 0, 0, NULL, 0.000, 100.000, 0);
 /*!40000 ALTER TABLE `entrada_nota` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela estoque_escolar.estoque
@@ -104,9 +101,9 @@ CREATE TABLE IF NOT EXISTS `estoque` (
 -- Copiando dados para a tabela estoque_escolar.estoque: 4 rows
 /*!40000 ALTER TABLE `estoque` DISABLE KEYS */;
 INSERT INTO `estoque` (`id_estoque`, `id_nota`, `id_produto`, `qtn_produto`) VALUES
-	(1, 0, 1, 11313),
-	(2, 0, 3, 4643),
-	(3, 0, 5, 227),
+	(1, 0, 1, 11395),
+	(2, 0, 3, 4648),
+	(3, 0, 5, 232),
 	(4, 0, 2, 8);
 /*!40000 ALTER TABLE `estoque` ENABLE KEYS */;
 
@@ -149,111 +146,106 @@ CREATE TABLE IF NOT EXISTS `info_produtos_entrada` (
   `qtd` decimal(10,2) NOT NULL,
   `valor` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id_info`)
-) ENGINE=MyISAM AUTO_INCREMENT=87 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=96 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela estoque_escolar.info_produtos_entrada: 35 rows
+-- Copiando dados para a tabela estoque_escolar.info_produtos_entrada: 2 rows
 /*!40000 ALTER TABLE `info_produtos_entrada` DISABLE KEYS */;
 INSERT INTO `info_produtos_entrada` (`id_info`, `id_nota`, `id_produto`, `data_validade`, `qtd`, `valor`) VALUES
-	(78, 17, 3, '0000-00-00', 4556.00, 454.00),
-	(77, 17, 3, '0000-00-00', 45.00, 56.00),
-	(64, 14, 1, '0000-00-00', 3.00, 34.00),
-	(75, 17, 1, '0000-00-00', 567.00, 4.00),
-	(74, 17, 1, '0000-00-00', 45.00, 4667.00),
-	(62, 14, 5, '0000-00-00', 6.00, 67.00),
-	(61, 14, 1, '0000-00-00', 5.00, 45.00),
-	(60, 13, 5, '0000-00-00', 4.00, 45.00),
-	(59, 13, 1, '0000-00-00', 4.00, 45.00),
-	(58, 13, 1, '0000-00-00', 4.00, 45.00),
-	(54, 0, 1, '0000-00-00', 4.00, 34.00),
-	(56, 12, 1, '0000-00-00', 2.00, 34.00),
-	(52, 0, 3, '0000-00-00', 34.00, 3.00),
-	(51, 0, 5, '0000-00-00', 3.00, 45.00),
-	(48, 11, 1, '0000-00-00', 34.00, 645.00),
-	(47, 11, 3, '0000-00-00', 3.00, 6.00),
-	(40, 8, 1, '0000-00-00', 2.00, 23.00),
-	(66, 16, 5, '0000-00-00', 200.00, 251.00),
-	(65, 15, 1, '0000-00-00', 12.00, 350.00),
-	(63, 14, 2, '0000-00-00', 4.00, 45.00),
-	(72, 17, 1, '0000-00-00', 4.00, 45.00),
-	(57, 12, 5, '0000-00-00', 4.00, 45.00),
-	(73, 17, 1, '0000-00-00', 567.00, 45.00),
-	(76, 17, 1, '0000-00-00', 10000.00, 10000.00),
-	(46, 11, 1, '0000-00-00', 6.00, 56.00),
-	(45, 11, 5, '0000-00-00', 3.00, 34.00),
-	(55, 0, 5, '0000-00-00', 4.00, 45.00),
-	(79, 17, 1, '0000-00-00', 4.00, 4.00),
-	(80, 17, 1, '0000-00-00', 4.00, 4.00),
-	(81, 17, 3, '0000-00-00', 4.00, 4.00),
-	(82, 17, 3, '0000-00-00', 4.00, 4.00),
-	(83, 17, 1, '0000-00-00', 10.00, 10.00),
-	(84, 17, 1, '0000-00-00', 54.00, 45.00),
-	(85, 17, 5, '0000-00-00', 4.00, 45.00),
-	(86, 0, 1, '0000-00-00', 34.00, 0.00);
+	(95, 24, 3, '0000-00-00', 5.00, 10.00),
+	(94, 24, 1, '0000-00-00', 10.00, 5.00);
 /*!40000 ALTER TABLE `info_produtos_entrada` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela estoque_escolar.item_cardapio
 CREATE TABLE IF NOT EXISTS `item_cardapio` (
   `id_item_card` int(11) NOT NULL AUTO_INCREMENT,
   `id_produto` int(11) DEFAULT NULL,
-  `qtd_mat` decimal(10,2) DEFAULT '0.00',
-  `qtd_vesp` decimal(10,2) DEFAULT '0.00',
-  `qtd_not` decimal(10,2) DEFAULT '0.00',
+  `qtd_mat` int(11) DEFAULT NULL,
+  `qtd_vesp` int(11) DEFAULT NULL,
+  `qtd_not` int(11) DEFAULT NULL,
   `id_cardapio` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_item_card`) USING BTREE
-) ENGINE=MyISAM AUTO_INCREMENT=50 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=166 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela estoque_escolar.item_cardapio: 49 rows
+-- Copiando dados para a tabela estoque_escolar.item_cardapio: 77 rows
 /*!40000 ALTER TABLE `item_cardapio` DISABLE KEYS */;
 INSERT INTO `item_cardapio` (`id_item_card`, `id_produto`, `qtd_mat`, `qtd_vesp`, `qtd_not`, `id_cardapio`) VALUES
-	(1, 5, 5.00, 0.00, 0.00, NULL),
-	(2, 1, 0.00, 0.00, 0.00, NULL),
-	(3, 3, 3.00, 4.00, 5.00, NULL),
-	(4, 3, 3.00, 4.00, 5.00, NULL),
-	(5, 3, 3.00, 4.00, 5.00, NULL),
-	(6, 1, 5.00, 5.00, 5.00, NULL),
-	(7, 1, 5.00, 5.00, 5.00, NULL),
-	(8, 1, 5.00, 5.00, 5.00, NULL),
-	(9, 1, 4.00, 45.00, 5.00, NULL),
-	(10, 1, 4.00, 45.00, 5.00, NULL),
-	(11, 1, 4.00, 45.00, 5.00, NULL),
-	(12, 5, 4.00, 56.00, 5.00, NULL),
-	(13, 5, 4.00, 56.00, 5.00, NULL),
-	(14, 5, 4.00, 56.00, 5.00, NULL),
-	(15, 5, 4.00, 56.00, 5.00, NULL),
-	(16, 5, 4.00, 56.00, 5.00, NULL),
-	(17, 5, 4.00, 56.00, 5.00, NULL),
-	(18, 5, 4.00, 56.00, 5.00, NULL),
-	(19, 5, 4.00, 56.00, 5.00, NULL),
-	(20, 1, 4.00, 6.00, 5.00, NULL),
-	(21, 1, 3.00, 4.00, 5.00, NULL),
-	(22, 1, 3.00, 4.00, 5.00, NULL),
-	(23, 1, 3.00, 4.00, 5.00, NULL),
-	(24, 1, 3.00, 4.00, 5.00, NULL),
-	(25, 1, 456.00, 456.00, 5456.00, NULL),
-	(26, 5, 4.00, 66.00, 6.00, NULL),
-	(27, 5, 4.00, 5.00, 6.00, NULL),
-	(28, 5, 4.00, 5.00, 6.00, NULL),
-	(29, 1, 5.00, 5.00, 6.00, NULL),
-	(30, 1, 5.00, 5.00, 6.00, NULL),
-	(31, 5, 5.00, 3.00, 4.00, NULL),
-	(32, 1, 55.00, 4.00, 5.00, NULL),
-	(33, 1, 55.00, 4.00, 5.00, NULL),
-	(34, 1, 55.00, 4.00, 5.00, NULL),
-	(35, 1, 55.00, 4.00, 5.00, NULL),
-	(36, 1, 5.00, 4.00, 3.00, NULL),
-	(37, 1, 5.00, 4.00, 3.00, NULL),
-	(38, 1, 5.00, 4.00, 3.00, NULL),
-	(39, 1, 56.00, 56.00, 56.00, NULL),
-	(40, 1, 56.00, 56.00, 5.00, NULL),
-	(41, 1, 56.00, 56.00, 5.00, NULL),
-	(42, 1, 56.00, 56.00, 5.00, NULL),
-	(43, 1, 56.00, 56.00, 5.00, NULL),
-	(44, 1, 56.00, 56.00, 5.00, 21),
-	(45, 1, 56.00, 56.00, 5.00, 21),
-	(46, 1, 56.00, 56.00, 5.00, 21),
-	(47, 1, 56.00, 56.00, 5.00, 21),
-	(48, 1, 56.00, 56.00, 5.00, 21),
-	(49, 3, 45.00, 65.00, 56.00, 22);
+	(1, 5, 5, 0, 0, NULL),
+	(2, 1, 0, 0, 0, NULL),
+	(3, 3, 3, 4, 5, NULL),
+	(4, 3, 3, 4, 5, NULL),
+	(5, 3, 3, 4, 5, NULL),
+	(6, 1, 5, 5, 5, NULL),
+	(7, 1, 5, 5, 5, NULL),
+	(8, 1, 5, 5, 5, NULL),
+	(9, 1, 4, 45, 5, NULL),
+	(10, 1, 4, 45, 5, NULL),
+	(11, 1, 4, 45, 5, NULL),
+	(12, 5, 4, 56, 5, NULL),
+	(13, 5, 4, 56, 5, NULL),
+	(14, 5, 4, 56, 5, NULL),
+	(15, 5, 4, 56, 5, NULL),
+	(16, 5, 4, 56, 5, NULL),
+	(17, 5, 4, 56, 5, NULL),
+	(18, 5, 4, 56, 5, NULL),
+	(19, 5, 4, 56, 5, NULL),
+	(20, 1, 4, 6, 5, NULL),
+	(21, 1, 3, 4, 5, NULL),
+	(22, 1, 3, 4, 5, NULL),
+	(23, 1, 3, 4, 5, NULL),
+	(24, 1, 3, 4, 5, NULL),
+	(25, 1, 456, 456, 5456, NULL),
+	(26, 5, 4, 66, 6, NULL),
+	(27, 5, 4, 5, 6, NULL),
+	(28, 5, 4, 5, 6, NULL),
+	(29, 1, 5, 5, 6, NULL),
+	(30, 1, 5, 5, 6, NULL),
+	(31, 5, 5, 3, 4, NULL),
+	(32, 1, 55, 4, 5, NULL),
+	(33, 1, 55, 4, 5, NULL),
+	(34, 1, 55, 4, 5, NULL),
+	(35, 1, 55, 4, 5, NULL),
+	(36, 1, 5, 4, 3, NULL),
+	(37, 1, 5, 4, 3, NULL),
+	(38, 1, 5, 4, 3, NULL),
+	(39, 1, 56, 56, 56, NULL),
+	(40, 1, 56, 56, 5, NULL),
+	(41, 1, 56, 56, 5, NULL),
+	(42, 1, 56, 56, 5, NULL),
+	(43, 1, 56, 56, 5, NULL),
+	(126, 1, 500, 400, 300, 28),
+	(148, 13, 45, 6, 5, 21),
+	(128, 7, 4, 6, 7, 28),
+	(49, 3, 45, 65, 56, 22),
+	(50, 5, 5, 5, 5, 24),
+	(51, 1, 34, 5, 6, 24),
+	(52, 3, 4, 6, 5, 24),
+	(85, 3, 6, 5, 6, 25),
+	(84, 5, 6, 7, 6, 25),
+	(111, 0, 0, 0, 0, 21),
+	(125, 5, 450, 300, 200, 28),
+	(127, 6, 300, 250, 200, 28),
+	(144, 1, 5, 7, 8, 21),
+	(146, 2, 56, 67, 56, 21),
+	(130, 1, 300, 300, 200, 30),
+	(131, 1, 4, 6, 45, 12),
+	(147, 9, 45, 6, 7, 21),
+	(132, 2, 4, 6, 7, 12),
+	(150, 1, 56, 7, 8, 5),
+	(149, 7, 45, 6, 7, 21),
+	(151, 7, 4, 5, 6, 5),
+	(152, 12, 4, 5, 6, 5),
+	(153, 5, 4, 6, 5, 5),
+	(154, 8, 45, 65, 45, 5),
+	(155, 14, 45, 66, 55, 5),
+	(156, 10, 444, 555, 444, 5),
+	(157, 6, 45, 67, 67, 31),
+	(158, 1, 5, 7, 6, 32),
+	(159, 1, 450, 500, 300, 33),
+	(160, 6, 300, 300, 300, 33),
+	(162, 1, 350, 300, 200, 35),
+	(163, 5, 250, 200, 150, 35),
+	(164, 3, 150, 100, 200, 35),
+	(165, 7, 30, 20, 10, 35);
 /*!40000 ALTER TABLE `item_cardapio` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela estoque_escolar.produtos
@@ -266,16 +258,25 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   `id_fornecedor` int(11) NOT NULL,
   `id_recurso` int(11) NOT NULL,
   PRIMARY KEY (`id_produto`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela estoque_escolar.produtos: 5 rows
+-- Copiando dados para a tabela estoque_escolar.produtos: 14 rows
 /*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
 INSERT INTO `produtos` (`id_produto`, `nome_produto`, `unidade_medida`, `data_validade`, `id_nota`, `id_fornecedor`, `id_recurso`) VALUES
 	(1, 'Arroz', 'kg', '0002-05-06', 0, 0, 0),
 	(2, 'Arroz 5', 'kg', '0000-00-00', 0, 0, 0),
 	(3, 'Batata', 'kg', '2022-12-10', 56, 1, 0),
 	(4, 'Batata', 'kg', '2022-02-20', 4, 9, 2),
-	(5, 'Feijao', 'kg', '2022-03-04', 2, 4, 2);
+	(5, 'Feijao', 'kg', '2022-03-04', 2, 4, 2),
+	(6, 'carne', 'kg', '0000-00-00', 0, 0, 0),
+	(7, 'alface', '', '0000-00-00', 0, 0, 0),
+	(8, 'uva', '', '0000-00-00', 0, 0, 0),
+	(9, 'abacate', '', '0000-00-00', 0, 0, 0),
+	(10, 'azeitona', '', '0000-00-00', 0, 0, 0),
+	(11, 'amora', '', '0000-00-00', 0, 0, 0),
+	(12, 'abacaxi', '', '0000-00-00', 0, 0, 0),
+	(13, 'azeite', '', '0000-00-00', 0, 0, 0),
+	(14, 'ameixa', '', '0000-00-00', 0, 0, 0);
 /*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela estoque_escolar.recurso
@@ -308,7 +309,7 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
 -- Copiando dados para a tabela estoque_escolar.usuarios: 1 rows
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` (`id`, `usuario`, `nome`, `senha`, `nivel`, `desativado`, `token`) VALUES
-	(0, 'Nat', 'Natallia', '1234', 0, 0, 'ed009dffdf8c824c603abd43e4787f17');
+	(0, 'Nat', 'Natallia', '1234', 0, 0, '23fcd6f5b85066e5e9ebe486b1ab773b');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
